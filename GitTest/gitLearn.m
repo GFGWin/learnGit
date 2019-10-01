51359f2a0dd1e55bcc667ac9aaf21a875a5fa95d
GFGWin <GFGWin@163.com>
2019-10-01 21:15:40 +0800

    GFG

---
+++ GitTest/gitLearn.m
@@ -0,0 +1,20 @@
+//
+//  gitLearn.m
+//  GitTest
+//
+//  Created by 谷富国 on 2019/10/1.
+//  Copyright © 2019 GFGWin. All rights reserved.
+//
+
+#import "gitLearn.h"
+
+@implementation gitLearn
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        NSLog(@"这是在GFG分支创建");
+    }
+    return self;
+}
+@end
